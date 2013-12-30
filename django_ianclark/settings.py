466675c2b88d8e2858fb745b8d7df3aed683909d
Ian Clark <ian.clark.001@gmail.com>
2013-12-30 13:00:13 +0000

    Updating settings.py to pull from production_settings.py module if available.
    * This will allow the production server to use a postgres DB whilst development can use sqlite and have debugging.
    * Updated root .gitignore to include .sqlite files
    * Added new .gitignore to ignore any production_settings.py files.

--- django_ianclark/settings.py
+++ django_ianclark/settings.py
@@ -12,20 +12,36 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+try:
+    from production_settings import *
+except ImportError:
+    # Quick-start development settings - unsuitable for production
+    # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = 'my$z5f#8qujyl(&4$b=xip9l-a*c$x$&=6jq5@@0n**oa1p&2p'
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'my$z5f#8qujyl(&4$b=xip9l-a*c$x$&=6jq5@@0n**oa1p&2p'
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+    TEMPLATE_DEBUG = True
 
-TEMPLATE_DEBUG = True
+    ALLOWED_HOSTS = []
 
-ALLOWED_HOSTS = []
+    # Database
+    # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
+    
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+STATIC_URL = '/static/'
 
 # Application definition
 
@@ -51,17 +67,6 @@ ROOT_URLCONF = 'django_ianclark.urls'
 
 WSGI_APPLICATION = 'django_ianclark.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
@@ -74,9 +79,3 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
-STATIC_URL = '/static/'
