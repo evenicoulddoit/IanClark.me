06d3cabe3a99407d0a4c7b942b12a3db870ad5f5
Ian Clark <ian.clark.001@gmail.com>
2014-01-02 21:56:16 +0000

    Adding a project-wide static files directory. Creating the structure of that directory. Also creating a SASS folder which will automatically generate to CSS and store in static/css

--- django_ianclark/settings.py
+++ django_ianclark/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROJECT_DIR = os.path.dirname(__file__)
 
 
 # Applications
@@ -70,6 +71,9 @@ DATABASES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_DIR, "static"),
+)
 
 # Look for production settings
 try:
