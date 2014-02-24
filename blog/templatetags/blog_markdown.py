cb6e933127fee9ca71a6d46ffc0db9822142a494
Ian Clark <ian.clark.001@gmail.com>
2014-02-24 21:52:41 +0000

    First pass at a blog view. Markdown introduced
    * Creates a VERY basic blog view
    * An index page just loops through all of the articles and provides a
    link
    * The post page just displays the title and translates the content to
    markdown.
    * The markdown was really the majority of the work
    * Markdown is translated to HTML using the `blog_markdown` filter
    * This required installing Markdown, Pygments and BeautifulSoup
        * Pygments allows me to color-code code snippets
          * Used Base 16 Ocean Dark theme
        * Beautifulsoup allows me to wrap markdown generated tables in a
    div. I use this to force a maximum width on tables
    * Code snippets also use the "Ubuntu Mono" font

---
+++ blog/templatetags/blog_markdown.py
@@ -0,0 +1,40 @@
+import markdown
+
+from bs4 import BeautifulSoup
+
+from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.encoding import force_unicode
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.filter(is_safe=True)
+@stringfilter
+def blog_markdown(value):
+    table_classname = "code-snippet"
+
+    extensions = ["nl2br", "fenced_code", "codehilite", "tables"]
+    extension_configs = {
+        'codehilite': [
+            ("linenums", True),
+            ("css_class", table_classname),
+        ]
+    }
+
+    md = markdown.markdown(force_unicode(value),
+                           extensions,
+                           extension_configs=extension_configs,
+                           safe_mode=True,
+                           output_format="html5",
+                           enable_attributes=False)
+
+    soup = BeautifulSoup(md)
+    tables = soup.find_all("table")
+
+    for table in tables:
+        responsive = soup.new_tag("div")
+        responsive["class"] = "table-responsive"
+        table.wrap(responsive)
+
+    return mark_safe(soup.prettify(formatter="html"))
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
-from django.shortcuts import render_to_response
+from django.shortcuts import render, render_to_response, get_object_or_404
+from blog.models import Post
 
 def index(request):
-    return render_to_response('blog/index.html', locals())
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', locals())
+
+def post(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, 'blog/post.html', locals())
