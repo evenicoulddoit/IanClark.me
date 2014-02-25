5acb28655c19c49071b81b9fbb8d272ae7891af9
Ian Clark <ian.clark.001@gmail.com>
2014-02-25 15:58:51 +0000

    Raises a 404 if blog post is unpublished and no "show_unpub" get value set

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.core.urlresolvers import reverse
+from django.http import Http404
 from django.shortcuts import render, render_to_response, get_object_or_404
 from blog.models import Post
 
@@ -12,4 +13,8 @@ def post(request, slug):
     post = get_object_or_404(Post, slug=slug)
     page_title = post.title
     page_url = post.get_absolute_url()
+
+    if not post.published and not "show_unpub" in request.GET:
+        raise Http404
+
     return render(request, 'blog/post.html', locals())
