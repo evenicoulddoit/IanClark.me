2cd7c46e53f816ebb3341e7ba82798b74bd4af50
Ian Clark <ian.clark.001@gmail.com>
2014-02-25 18:24:56 +0000

    Updates admin model to ensure that tags are always saved comma delimited

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from blog.models import Post
+import re
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     search_fields = ['title', 'description', 'content']
     list_display = ['title', 'description']
     list_filter = ['published', 'created']
+
+    def save_model(self, request, obj, form, change):
+        tags_processed = filter(None, re.split(r'[;,|]', obj.tags))
+        tags_processed = ", ".join(tag.strip().lower() for tag in tags_processed)
+
+        obj.tags = tags_processed
+        obj.save()
 
 admin.site.register(Post, PostAdmin)
