98482922389db30f83a30218420268b29c4868f0
Ian Clark <ian.clark.001@gmail.com>
2014-02-25 13:39:48 +0000

    First pass at SEO meta tags and info
    * Adds support for OG, Twitter and G+
    * Adds some basic images to use on those sites
    * If a `page_url` is set, then we post it as being the canonical version
    of the page
    * At the moment sets some defaults in the templates. Not sure if this is
    the best approach.
    * Also makes the page not resizable on mobile devices

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,12 @@ from blog.models import Post
 
 def index(request):
     posts = Post.objects.all()
+    page_title = "Blog"
+    page_url = "/blog"
     return render(request, 'blog/index.html', locals())
 
 def post(request, slug):
     post = get_object_or_404(Post, slug=slug)
+    page_title = post.title
+    page_url = post.get_absolute_url()
     return render(request, 'blog/post.html', locals())
