76e178439595370a02881d8dd6dbfe2f9b9877e1
Ian Clark <ian.clark.001@gmail.com>
2014-02-25 13:55:40 +0000

    Fixes circular redirect from blog URL missing trailing /. Uses reverse()

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
+from django.core.urlresolvers import reverse
 from django.shortcuts import render, render_to_response, get_object_or_404
 from blog.models import Post
 
 def index(request):
     posts = Post.objects.all()
     page_title = "Blog"
-    page_url = "/blog"
+    page_url = reverse('blog.views.index')
     return render(request, 'blog/index.html', locals())
 
 def post(request, slug):
