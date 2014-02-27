02b15c4cf9d60f398985e50cc801b42e836bf001
Ian Clark <ian.clark.001@gmail.com>
2014-02-27 18:22:36 +0000

    Adds Node page Grunt and Grunt-Closure-Compiler to compile JS automatically
    * Just run "grunt watch" to automatically watch the JS for changes and
    generate a minified output using Simple modifications
    * I've taken the small Angular app declaration out of the HTML and into
    main.js (in src/, minified into main.min.js in js/)

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+  
+  grunt.initConfig({
+    'watch': {
+      files: ['static/src/*'],
+      tasks: ['closure-compiler']
+    },
+    'closure-compiler': {
+      frontend: {
+        closurePath: 'bin/',
+        js: 'static/src/main.js',
+        jsOutputFile: 'static/js/main.min.js',
+        maxBuffer: 500,
+        options: {
+          compilation_level: 'SIMPLE_OPTIMIZATIONS',
+          language_in: 'ECMASCRIPT5_STRICT'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-closure-compiler');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['closure-compiler']);
+}
---
+++ static/src/main.js
@@ -0,0 +1,6 @@
+var ianClarkApp = angular.module('ianClarkApp', []);
+
+function BlogCtrl($scope, $http) { }
+BlogCtrl.$inject = ['$scope', '$http'];
+
+ianClarkApp.controller('BlogCtrl', BlogCtrl);
