628139c2b8d44ce6cef3b683a4b1ba13d91f5678
Ian Clark <ian.clark.001@gmail.com>
2014-02-28 13:51:18 +0000

    Decided to use jQuery rather than AngularJS
    I don't expect I'll be needing many scripts, and the small things I will
    want a probably better suited to doing using jQuery.

--- static/src/main.js
+++ static/src/main.js
@@ -1,6 +1,50 @@
-var ianClarkApp = angular.module('ianClarkApp', []);
+(function($) {
+  "use strict";
 
-function BlogCtrl($scope, $http) { }
-BlogCtrl.$inject = ['$scope', '$http'];
+  var blog = {
+    init: function() {
+      this.extract_dom();
+      this.events();
+    },
 
-ianClarkApp.controller('BlogCtrl', BlogCtrl);
+    /**
+     * Retrieve and store all valid jQuery elements from the DOM
+     */
+    extract_dom: function() {
+      this.$nav = $("#nav"),
+      this.$navToggle = $("#nav-toggle");
+    },
+
+    /**
+     * Prevent nav clicks from propagating to the body and removing it's active state
+     */
+    nav_click: function(e) {
+      e.stopPropagation();
+    },
+
+    /**
+     * Toggle the navigation, using an optional explicit boolean to set manually
+     */
+    nav_toggle: function(explicit) {
+      var newState = typeof explicit == "boolean" ? explicit : !this.$nav.hasClass("active");
+      this.$nav.toggleClass("active", newState);
+    },
+
+    /**
+     * Initialise events
+     */
+    events: function() {
+      $("body").on("click", $.proxy(this.nav_toggle, this, false));
+      this.$nav.on("click", $.proxy(this.nav_click, this));
+      this.$navToggle.on("click", $.proxy(this.nav_toggle, this));
+    }
+  };
+
+  /**
+   * Once the DOM is ready, initialise the blog
+   */
+  $("document").ready(function() {
+    blog.init();
+  });
+
+})(jQuery);
