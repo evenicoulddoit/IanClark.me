bfd23f5a01991a59b7bd962736baf94c6c310d22
Ian Clark <ian.clark.001@gmail.com>
2014-03-02 01:19:17 +0000

    Create a swish about page. So swish that it's now the "home" page
    \o/
    * About page MADE, and once I'd made it I realised it should be the home
    page

--- about/views.py
+++ about/views.py
@@ -3,6 +3,6 @@ from django.http import Http404
 from django.shortcuts import render, render_to_response, get_object_or_404
 
 def index(request):
-    page_title = "About"
+    page_title = "Home"
     page_url = reverse('about.views.index')
     return render(request, 'about/index.html', locals())
--- django_ianclark/urls.py
+++ django_ianclark/urls.py
@@ -7,9 +7,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'django_ianclark.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', include('blog.urls')),
+    url(r'^$', include('about.urls')),
     url(r'^blog/', include('blog.urls')),
     url(r'^experience/', include('experience.urls')),
-    url(r'^about/', include('about.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
