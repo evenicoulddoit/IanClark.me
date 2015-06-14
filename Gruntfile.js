b00ffe73e544fe138d00660b5ff333a3af904fe9
Ian Clark <ian.clark.001@gmail.com>
2015-06-14 12:37:21 +0100

    Making Sass automatically compiled

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,28 +1,42 @@
+"use strict";
+
 module.exports = function(grunt) {
+  var build = [
+    "closure-compiler",
+    "compass"
+  ];
 
   grunt.initConfig({
-    'watch': {
-      files: ['static/src/*'],
-      tasks: ['closure-compiler']
-    },
-    'closure-compiler': {
-      frontend: {
-        closurePath: 'bin/',
-        js: ['static/src/main.js',
-             'static/src/experience.js'],
-        jsOutputFile: 'static/js/main.min.js',
-        maxBuffer: 500,
+    "compass": {
+      dist: {
         options: {
-          compilation_level: 'SIMPLE_OPTIMIZATIONS',
-          language_in: 'ECMASCRIPT5_STRICT'
+          config: "config.rb"
         }
       }
-    }
+    },
+    "closure-compiler": {
+      frontend: {
+        closurePath: "bin/",
+        js: ["static/src/main.js",
+             "static/src/experience.js"],
+        jsOutputFile: "static/js/main.min.js",
+        maxBuffer: 500,
+        options: {
+          compilation_level: "SIMPLE_OPTIMIZATIONS",
+          language_in: "ECMASCRIPT5_STRICT"
+        }
+      }
+    },
+    "watch": {
+      files: ["static/src/*"],
+      tasks: ["closure-compiler"]
+    },
   });
 
-  grunt.loadNpmTasks('grunt-closure-compiler');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks("grunt-closure-compiler");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-compass");
 
-  grunt.registerTask('default', ['closure-compiler']);
-  grunt.registerTask('build', ['closure-compiler']);
-}
+  grunt.registerTask("default", build);
+  grunt.registerTask("build", build);
+};
